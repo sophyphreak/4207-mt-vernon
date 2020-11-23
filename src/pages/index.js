ba4d656399bceef5f5bdf79231294d3f7210025f
Andrew Horn <arhorn@smcm.edu>
2020-11-22 21:35:03 -0600

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,22 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h1>Community Meeting</h1>
+    <p><b>November 29th at 3pm on Google Meet</b></p>
+    <br />
+    <br />
+    <p>Google meet link:</p>
+    <p><a href="meet.google.com/fxq-sjki-twc">meet.google.com/fxq-sjki-twc</a></p>
+    <br />
+    <br />
+    <p>Agenda link:</p>
+    <p><a href="https://docs.google.com/document/d/1N3ZxceYRZR3P-qOXPp-JkKmqzurGxN3VEOqr971SzNA/edit?usp=sharing">https://docs.google.com/document/d/1N3ZxceYRZR3P-qOXPp-JkKmqzurGxN3VEOqr971SzNA/edit?usp=sharing</a></p>
+    <p>(please add anything you want to talk about)</p>
   </Layout>
 )
 
