1b15d9dc69487ab732a73b3966e7e01ef322c717
Andrew Horn <arhorn@smcm.edu>
2020-11-22 22:29:30 -0600

    with charlotte's advice

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,16 +7,24 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Community Meeting</h1>
-    <p><b>November 29th at 3pm on Google Meet</b></p>
     <br />
     <br />
-    <p>Google meet link:</p>
+    <h4>Meeting Date</h4>
+    <p>Sunday, November 29th</p>
+    <br/>
+    <h4>Meeting Time</h4>
+    <p>3 pm</p>
+    <br/>
+    <h4>Meeting Location</h4>
     <p><a href="meet.google.com/fxq-sjki-twc">meet.google.com/fxq-sjki-twc</a></p>
-    <br />
-    <br />
-    <p>Agenda link:</p>
+    <p>(click the link above to join)</p>
+    <br/>
+    <h4>Meeting Agenda</h4>
     <p><a href="https://docs.google.com/document/d/1N3ZxceYRZR3P-qOXPp-JkKmqzurGxN3VEOqr971SzNA/edit?usp=sharing">https://docs.google.com/document/d/1N3ZxceYRZR3P-qOXPp-JkKmqzurGxN3VEOqr971SzNA/edit?usp=sharing</a></p>
     <p>(please add anything you want to talk about)</p>
+    <br/>
+    <h4>Facilitating</h4>
+    <p>Andrew (#16)</p>
   </Layout>
 )
 
