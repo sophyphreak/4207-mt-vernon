f7c9b6d87bcb4f534facd79f32552163e364da7a
Andrew Horn <arhorn@smcm.edu>
2020-11-29 15:01:36 -0600

    corrected link

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,18 +11,26 @@ const IndexPage = () => (
     <br />
     <h4>Meeting Date</h4>
     <p>Sunday, November 29th</p>
-    <br/>
+    <br />
     <h4>Meeting Time</h4>
     <p>3 pm</p>
-    <br/>
+    <br />
     <h4>Meeting Location</h4>
-    <p><a href="meet.google.com/fxq-sjki-twc">meet.google.com/fxq-sjki-twc</a></p>
+    <p>
+      <a href="https://meet.google.com/fxq-sjki-twc">
+        meet.google.com/fxq-sjki-twc
+      </a>
+    </p>
     <p>(click the link above to join)</p>
-    <br/>
+    <br />
     <h4>Meeting Agenda</h4>
-    <p><a href="https://docs.google.com/document/d/1N3ZxceYRZR3P-qOXPp-JkKmqzurGxN3VEOqr971SzNA/edit?usp=sharing">https://docs.google.com/document/d/1N3ZxceYRZR3P-qOXPp-JkKmqzurGxN3VEOqr971SzNA/edit?usp=sharing</a></p>
+    <p>
+      <a href="https://docs.google.com/document/d/1N3ZxceYRZR3P-qOXPp-JkKmqzurGxN3VEOqr971SzNA/edit?usp=sharing">
+        https://docs.google.com/document/d/1N3ZxceYRZR3P-qOXPp-JkKmqzurGxN3VEOqr971SzNA/edit?usp=sharing
+      </a>
+    </p>
     <p>(please add anything you want to talk about)</p>
-    <br/>
+    <br />
     <h4>Facilitating</h4>
     <p>Andrew (#16)</p>
   </Layout>
